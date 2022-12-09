abda791e77666dbc02f851a8902d94fec369b7af
Santiago <santiago.gutierrez02@correo.usa.edu.co>
2022-12-09 16:27:27 -0600

    Se agregan comentarios al código

--- 2022/day02/day02_part01.py
+++ 2022/day02/day02_part01.py
@@ -1,4 +1,8 @@
 def get_play(turn):
+    '''
+    Función para calcular el puntaje obtenido en el turno
+    Recibe una tupla con las opciones del oponente[0] y de uno[1]
+    '''
     if ((turn[0] == 'A' and turn[1] == 1) or 
     (turn[0] == 'B' and turn[1] == 2) or
     (turn[0] == 'C' and turn[1] == 3)):
@@ -10,6 +14,11 @@ def get_play(turn):
     return 0
 
 def get_turn_points(result = []):
+    '''
+    Función para calcular los puntos de todos los turnos
+    Recibe una lista de tuplas con lo escogido en cada turno
+    '''
+    ### Se hace una suma de los resultados obtenidos al enviar cada elemento del result a la función get_play
     turn_points = sum(map(get_play, result))
     return turn_points
 
@@ -31,8 +40,7 @@ def main():
                 break
             result.append((line.split()[0], my_option[line.split()[1]]))
             total_points += result[-1][1]
-            #print(result[-1][0], result[-1][1])
-            #total_points = result[-1] + get_turn_points((result[-1][0], result[-1][1]))
+
     total_points += get_turn_points(result)
     print(total_points)
 
