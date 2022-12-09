9e85a9157eb9d62b5ded0599847932481132b77c
Santiago <santiago.gutierrez02@correo.usa.edu.co>
2022-12-09 16:21:06 -0600

    Se agrega solución de la parte 1 del día 2

---
+++ 2022/day02/day02_part01.py
@@ -0,0 +1,40 @@
+def get_play(turn):
+    if ((turn[0] == 'A' and turn[1] == 1) or 
+    (turn[0] == 'B' and turn[1] == 2) or
+    (turn[0] == 'C' and turn[1] == 3)):
+        return 3
+    if ((turn[0] == 'A' and turn[1] == 2) or 
+    (turn[0] == 'B' and turn[1] == 3) or
+    (turn[0] == 'C' and turn[1] == 1)):
+        return 6
+    return 0
+
+def get_turn_points(result = []):
+    turn_points = sum(map(get_play, result))
+    return turn_points
+
+def main():
+    # Dict con los valores obtenidos por nuestra shape escogida
+    my_option = {
+        'X': 1, # Rock
+        'Y': 2, # Paper 
+        'Z': 3  # Scissor
+    }
+    # list con los resultados totales
+    result = []
+    total_points = 0
+
+    with open('..\inputs\day02.txt') as f:
+        while True:
+            line = f.readline()
+            if not line:
+                break
+            result.append((line.split()[0], my_option[line.split()[1]]))
+            total_points += result[-1][1]
+            #print(result[-1][0], result[-1][1])
+            #total_points = result[-1] + get_turn_points((result[-1][0], result[-1][1]))
+    total_points += get_turn_points(result)
+    print(total_points)
+
+if __name__ == '__main__':
+    main()
